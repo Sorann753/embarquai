16695b92b5555747ab8700d585261026700871eb
Sorann753 <Arthusd@hotmail.fr>
2022-03-01 14:54:02 +0100

    création du projet et ajout des diagramme
    - ajout de la V1 du diagramme de block
    - ajout de la V1 du Requirement diagram
    - ajout du gitignore
    -

---
+++ M5Stack/src/main.cpp
@@ -0,0 +1,30 @@
+/**
+ * @brief fichier principal du M5Stack
+ * @author DORIATH ARTHUS
+ * @date 01/03/2022
+ * @version 0.1
+ */
+
+#include <Arduino.h>
+
+
+
+/**
+ * @brief fonction d'initialisation du programme
+ * @param none
+ * @return none
+ */
+void setup() {
+    // put your setup code here, to run once:
+}
+
+
+
+/**
+ * @brief boucle d'exécution du programme
+ * @param none
+ * @return none
+ */
+void loop() {
+    // put your main code here, to run repeatedly:
+}

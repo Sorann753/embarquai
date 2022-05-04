bd6c2db53e7368ff518ae5d64e0d9a4bc12d7192
Balerion14 <93737471+Balerion14@users.noreply.github.com>
2022-05-04 12:19:30 +0200

    header des fonction lorawan

---
+++ M5Stack/lib/lorawan_lib/include/fonction_send_lora.h
@@ -0,0 +1,56 @@
+#ifndef FONCTION_SEND_LORA_H 
+#define FONCTION_SEND_LORA_H 
+
+#include <M5Core2.h>
+#include <algorithm>
+
+/*@brief une structure qui stoque les données des capteurs d'un bateau */
+struct data_bateau{
+
+    //Fr
+};
+
+/**
+* @brief Fonction qui va envoyer une chaine de caractère sur un module lorawan.
+* @param data donnee structure ui contiendra les différents champs qui contiendront les données des capteurs
+* @return void 
+*/
+void Send_Message_Lorawan(const data_bateau& data);
+
+/**
+* @brief Fonction qui va initialiser le port serial 2 pour faire fonctionner le module lorawan
+* @param none
+* @return void
+*/
+void init_serial_2();
+
+/**
+ * @brief Generation d'un string contenant le message combinant une clé pour donner une valeur d'identification en cas de perte de données 
+ * @param key 
+ * @param data 
+ * @return std::string 
+ */
+String generation_CRC(const String& data, const String& key);
+
+/**
+ * @brief Conversion d'une chaine de caractère en binaire 
+ * @return std::string 
+ */
+String conversion_binaire(const String& data);
+
+/**
+ * @brief Fonction qui va faire un xor(ou exclusif) entre deux chaine de carectere
+ * @param a 
+ * @param b 
+ * @return std::string 
+ */
+String xor1(const String& a, const String& b);
+
+/**
+ * @brief Fonction qui va diviser les données binaires par la clé et stocker le reste de la division
+ * @param divident 
+ * @param divisor 
+ */
+String mod2div(const String& divident, const String& divisor);
+
+#endif

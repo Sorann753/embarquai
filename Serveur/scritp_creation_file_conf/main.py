e04fa7598992e8d5732291b47c93467d78354cab
Benaitier Loris <benaitier.prof.g@gmail.com>
2022-05-23 10:48:14 +0200

    Upgrade
    Remove part default

--- Serveur/scritp_creation_file_conf/main.py
+++ Serveur/scritp_creation_file_conf/main.py
@@ -1,8 +1,5 @@
 import configparser
 config = configparser.ConfigParser()
-config['DEFAULT'] = {'port': '1883',
-                     'ip': '127.0.0.1',
-                     'nom_bdd': 'bdd_embarquai.sqlite'}
 config['RESEAU'] = {'port': 'Remplacer champ(uniquement chiffres ou nombres)',
                      'ip': 'Remplacer champ(uniquement chiffres ou nombres)'}
 config['BDD'] = {'nom_bdd': 'Remplacer champ(uniquement chiffres ou nombres)'}

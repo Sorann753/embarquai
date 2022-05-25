d27c401eb8b99d08f0ba27b3a9cd587b6b698457
Benaitier Loris <benaitier.prof.g@gmail.com>
2022-05-25 10:40:33 +0200

    Add script bash
    Instalation module python
    paho-mqtt

---
+++ Serveur/script_instalation_module_python/Installation_module_mqtt.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Lancer commande pour installer paho-mqtt
+# Mode d'emploi : 
+# Commande de lancement : bash nom_du_fichier (Il faut se trouver dans le dossier ou se trouve le script)
+pip3 install paho-mqtt

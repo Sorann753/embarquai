f230f5eb12bf3082030e91fac5f0dde6f54d8d15
Balerion14 <benaitier.prof.g@gmail.com>
2022-05-29 22:03:50 +0200

    mise a jour commentaire python

--- Serveur/client_python_test/main.py
+++ Serveur/client_python_test/main.py
@@ -1,35 +1,63 @@
-import time
+"""Client de test pour se connecter en mqtt et publier des messages.
 
+Usage:
+======
+    python main.py 
+
+"""
+
+__authors__ = ("Loris Benaitier")
+__copyright__ = "MIT"
+__date__ = "2022-05-01"
+__version__= "1.0"
+
+# Bibliotèques
+import time
 import paho.mqtt.client as mqtt
 
-# The callback for when the client receives a CONNACK response from the server.
+# Le rappel lorsque le client reçoit une réponse lora du serveur.
 def on_connect(client, userdata, flags, rc):
+    """! Fonction qu'on appelle lorsque le client reçoit une réponse lora du serveur
+    @param client Identité du client
+    @param userdata Données définies par l'utilisateur de tout type qui sont transmises en tant que paramètre userdata aux rappels
+    @param flag objet dictionnaire utilisé pour vérifier si vous avez défini une session propre sur True ou False pour l'objet client
+    @param rc code de résultat , est utilisé pour vérifier l'état de la connexion
+
+    @return  rien
+    """
     print("Connected with result code " + str(rc))
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
+    # S'abonner à on_connect() signifie que si nous perdons la connexion et
+    # se reconnecter puis les abonnements seront renouvelés.
     client.subscribe("lora/#")
 
-# The callback for when a PUBLISH message is received from the server.
+# Le rappel lorsqu'un message PUBLISH est reçu du serveur.
 def on_message(client, userdata, msg):
+    """! Fonction qu'on appelle lorsqu'un message PUBLISH est reçu du serveur
+    @param client Identité du client
+    @param userdata Données définies par l'utilisateur de tout type qui sont transmises en tant que paramètre userdata aux rappels
+    @param msg Contient la donnée
+
+    @return rien
+    """
     topic = str(msg.topic)
     message = str(msg.payload.decode("utf-8"))
     print(topic + " " + message)
 
+
+# Appel pour voir si il y a des messages publié ou non
 client = mqtt.Client()
 
-
+# Rentrer l'IP et le port ou le client doit ecouter
 client.connect("192.168.1.156", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-# With loop_start you have to write the while loop yourself but the programma will continue
+# Appel bloquant qui traite le trafic réseau, distribue les rappels et
+# gère la reconnexion.
+# D'autres fonctions loop*() sont disponibles qui donnent une interface threadée et un
+# interface manuelle.
+"""Boucle qui envoi à l'infini la data suivante avec un delai de 10 seconde"""
 while True:
     your_data = "/YidAMTAuMDsxNS4wOzIwLjUhMTAxMDEwMDB+Jw==/up"
     client.publish("lora/",your_data,)
     time.sleep(10)
-client.loop_forever()     # loop_forever acts as a while True loop (the program will not continue past here).
+client.loop_forever()     # (le programme ne continuera pas au-delà d'ici).
 
-
-# how to publish

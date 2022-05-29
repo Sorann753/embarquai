3d7626f3a4fe444bd27304e770eee12042535427
Balerion14 <benaitier.prof.g@gmail.com>
2022-05-30 00:39:46 +0200

    add dioxygen

--- Serveur/script_instalation_module_python/Installation_module_mqtt.bash
+++ Serveur/script_instalation_module_python/Installation_module_mqtt.bash
@@ -1,4 +1,13 @@
 #!/bin/bash
+# -*- tab-width: 4; encoding: utf-8 -*-
+#
+## @file
+## @author Loris Benaitier
+## @brief Bash pour installer paho-mqtt
+## @copyright MIT
+## @version 1
+##
+
 # Lancer commande pour installer paho-mqtt
 # Mode d'emploi : 
 # Commande de lancement : bash nom_du_fichier (Il faut se trouver dans le dossier ou se trouve le script)

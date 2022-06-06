f4b73d0ab12b7b0d1efea35086833aabf342fcfd
Balerion14 <benaitier.prof.g@gmail.com>
2022-06-06 18:50:18 +0200

    Upgrade instructions script bash

--- Serveur/script_instalation_module_python/Installation_module_mqtt.bash
+++ Serveur/script_instalation_module_python/Installation_module_mqtt.bash
@@ -10,5 +10,10 @@
 
 # Lancer commande pour installer paho-mqtt
 # Mode d'emploi : 
-# Commande de lancement : bash nom_du_fichier (Il faut se trouver dans le dossier ou se trouve le script)
+# Commande de lancement d'un script bash : 
+# 1/ sudo chmod +x nom_fichier.bash 
+# 2/ sudo sed -i -e 's/\r$//' nom_fichier.bash (Lancer cette commande si on a l'erreur suivant : bad interpreter : No such file or directory)
+# 3/ sudo ./nom_fichier.bash (Il faut se trouver dans le dossier ou se trouve le script)
 pip3 install paho-mqtt
+# Si il manque d'autres modules (erreur quand on lance le serveur python : No module named 'nom_module_manquant')
+# installer module manquant : pip3 install nom_module_manquant
